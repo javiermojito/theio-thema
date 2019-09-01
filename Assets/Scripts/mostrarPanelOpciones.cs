cad0619241a4bfec510b265354469c6ffa064e82
Javico <jcollazosgomez@gmail.com>
2019-09-01 18:04:56 -0500

    Boton opciones revela el que será el panel opciones

---
+++ Assets/Scripts/mostrarPanelOpciones.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mostrarPanelOpciones : MonoBehaviour
+{
+
+    public GameObject panel;
+
+
+    public void mostrarPanel()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(!panel.activeSelf);
+        }
+        
+    }
+}

5c630a92283e3a4195ccfc23153300b789f2beeb
Javico <jcollazosgomez@gmail.com>
2019-09-01 20:47:15 -0500

    Retoques sobre escenario, nada funcional

---
+++ Assets/Scripts/movimientoCamaraEntrenamiento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoCamaraEntrenamiento : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
